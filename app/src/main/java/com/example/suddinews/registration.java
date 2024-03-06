3af6a2a0517aa007fe1ad95d8012e7d6041452ab
swapnildk23 <swapnilkore5027@gmail.com>
2024-03-06 12:52:41 +0530

    Commit3

---
+++ app/src/main/java/com/example/suddinews/registration.java
@@ -0,0 +1,27 @@
+package com.example.suddinews;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class registration extends AppCompatActivity {
+        AppCompatButton btn_reg;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+    }
+    public void backtologin(View view)
+    {
+        final Intent i=new Intent(this,MainActivity.class);
+        startActivity(i);
+    }
+    private void BindUI()
+    {
+        btn_reg=findViewById(R.id.btn_reg);
+    }
+}
