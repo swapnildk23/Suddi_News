0d4a42971e7eeb67e3f0093a65ad538c62c10219
swast735 <133983059+swast735@users.noreply.github.com>
2024-03-06 10:33:21 +0530

    commit_2

---
+++ app/src/main/java/com/example/suddinews/News.kt
@@ -0,0 +1,11 @@
+package com.example.suddinews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class News : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news)
+    }
+}
