488ce795201cd850f5d2bed431d8d204b2520c04
swapnildk23 <swapnilkore5027@gmail.com>
2024-03-08 21:59:34 +0530

    Commit 6

--- app/src/main/java/com/example/suddinews/newsupload.kt
+++ app/src/main/java/com/example/suddinews/newsupload.kt
@@ -7,5 +7,6 @@ class newsupload : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_newsupload)
+
     }
 }
