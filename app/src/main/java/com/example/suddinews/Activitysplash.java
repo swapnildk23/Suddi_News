471ad5e58eb605c6adf9fbbc0c68769e8654b646
swapnildk23 <swapnilkore5027@gmail.com>
2024-03-10 10:37:57 +0530

    Commit 9

--- app/src/main/java/com/example/suddinews/Activitysplash.java
+++ app/src/main/java/com/example/suddinews/Activitysplash.java
@@ -3,14 +3,28 @@ package com.example.suddinews;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ActivityOptions;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+
 public class Activitysplash extends AppCompatActivity {
     private RelativeLayout ly;
+    private ProgressBar loadingProgressBar;
+    private TextView internetTextView;
+    private boolean internetConnected = false;
+    private boolean mainActivityStarted = false;
+
+    private Handler handler;
+    private Runnable runnable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,20 +34,57 @@ public class Activitysplash extends AppCompatActivity {
         // Call UI binding function
         BindUi();
 
-        final Intent i = new Intent(this, MainActivity.class);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Bundle b = ActivityOptions.makeSceneTransitionAnimation(Activitysplash.this).toBundle();
-                startActivity(i, b);
-                finish();
-            }
-        }, 1000);
+        // Start checking internet connectivity
+        startCheckingInternetConnectivity();
     }
 
     // Bind UI elements with the respective component variables
-
     private void BindUi() {
         ly = findViewById(R.id.lay);
+        loadingProgressBar = findViewById(R.id.loadingProgressBar);
+        internetTextView = findViewById(R.id.internetTextView);
+    }
+
+    // Method to start checking internet connectivity
+    private void startCheckingInternetConnectivity() {
+        handler = new Handler();
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                checkInternetConnectivity();
+                handler.postDelayed(this, 1000); // Check every second
+            }
+        };
+        handler.postDelayed(runnable, 1000); // Initial delay
+    }
+
+    // Method to stop checking internet connectivity
+    private void stopCheckingInternetConnectivity() {
+        handler.removeCallbacks(runnable);
+    }
+
+    // Method to check internet connectivity
+    private void checkInternetConnectivity() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivityManager != null) {
+            NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+            internetConnected = activeNetworkInfo != null && activeNetworkInfo.isConnected();
+            if (internetConnected&&!mainActivityStarted) {
+                stopCheckingInternetConnectivity(); // Stop checking once internet connected
+                startMainActivity();
+            } else {
+                loadingProgressBar.setVisibility(View.VISIBLE);
+                internetTextView.setVisibility(View.VISIBLE);
+            }
+        }
+    }
+
+    // Method to start the MainActivity
+    private void startMainActivity() {
+        final Intent i = new Intent(this, MainActivity.class);
+        Bundle b = ActivityOptions.makeSceneTransitionAnimation(Activitysplash.this).toBundle();
+        startActivity(i, b);
+        finish();
+        mainActivityStarted=true;
     }
 }
--- app/src/main/java/com/example/suddinews/MainActivity.java
+++ app/src/main/java/com/example/suddinews/MainActivity.java
@@ -1,37 +1,180 @@
 package com.example.suddinews;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
 import android.content.Intent;
 import android.mtp.MtpConstants;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.database.annotations.NotNull;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
-    AppCompatButton btn_login,btn_next;
+    AppCompatButton btn_login, btn_next;
+    EditText phoneno, otp;
+    FirebaseAuth mAuth;
+    String verificationId;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BindUI();
+        mAuth = FirebaseAuth.getInstance();
+        btn_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phoneNumber = phoneno.getText().toString();
+                if ((TextUtils.isEmpty(phoneno.getText().toString()))) {
+                    Toast.makeText(MainActivity.this, "Enter Valid Phone Number", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    if (phoneNumber.length() < 10) {
+                        Toast.makeText(MainActivity.this, "Enter Valid Phone Number", Toast.LENGTH_SHORT).show();
+                    } else {
+                        sendverificationcode(phoneNumber);
+                    }
+                }
+
+            }
+        });
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if ((TextUtils.isEmpty(otp.getText().toString()))) {
+                    Toast.makeText(MainActivity.this, "Enter Valid Phone Number", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    verifycode(otp.getText().toString());
+                }
+            }
+        });
     }
+
+    private void sendverificationcode(String phoneNumber) {
+        PhoneAuthOptions options =
+                PhoneAuthOptions.newBuilder(mAuth)
+                        .setPhoneNumber("+91" + phoneNumber)            // Phone number to verify
+                        .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                        .setActivity(this)                 // Activity (for callback binding)
+                        .setCallbacks(mCallBack)           // OnVerificationStateChangedCallbacks
+                        .build();
+        PhoneAuthProvider.verifyPhoneNumber(options);
+
+    }
+
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks
+            mCallBack = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+        // below method is used when
+        // OTP is sent from Firebase
+        @Override
+        public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken token) {
+            super.onCodeSent(s, token);
+            // when we receive the OTP it
+            // contains a unique id which
+            // we are storing in our string
+            // which we have already created.
+            verificationId = s;
+            Toast.makeText(MainActivity.this,"OTP Sent",Toast.LENGTH_SHORT).show();
+            btn_login.setEnabled(true);
+        }
+
+        // this method is called when user
+        // receive OTP from Firebase.
+        @Override
+        public void onVerificationCompleted(@NotNull PhoneAuthCredential phoneAuthCredential) {
+            // below line is used for getting OTP code
+            // which is sent in phone auth credentials.
+            final String code = phoneAuthCredential.getSmsCode();
+
+            // checking if the code
+            // is null or not.
+            if (code != null) {
+                // after setting this code
+                // to OTP edittext field we
+                // are calling our verifycode method.
+                verifycode(code);
+            }
+        }
+
+        // this method is called when firebase doesn't
+        // sends our OTP code due to any error or issue.
+        @Override
+        public void onVerificationFailed(@NotNull FirebaseException e) {
+            // displaying error message with firebase exception.
+            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    };
+
+    // below method is use to verify code from Firebase.
+    private void verifycode(String code) {
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationId, code);
+        signInWithCredential(credential);
+    }
+
+    private void signInWithCredential(PhoneAuthCredential credential) {
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                         if(task.isSuccessful()){
+                            Toast.makeText(MainActivity.this,"Login Successful",Toast.LENGTH_SHORT).show();
+                            /*
+                            Code for saving phone number and checking and deciding whether to go to registration page or
+                            home page shoul be here and make sure to change the startActivity as required
+                             */
+                            startActivity(new Intent(MainActivity.this,registration.class));
+                         }
+                    }
+                });
+    }
+
+
+
+
+
     public void admin(View view)
     {
         Intent j = new Intent(this, adminlogin.class);
         startActivity(j);
     }
-    public void logi(View view)
-    {
-        Intent i = new Intent(this, News.class);
-        startActivity(i);
-    }
+
     private void BindUI()
     {
         btn_login=findViewById(R.id.btn_login);
         btn_next=findViewById(R.id.btn_next);
+        phoneno=findViewById(R.id.login_phone);
+        otp=findViewById(R.id.login_otp);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if(currentUser!=null)
+        {
+            startActivity(new Intent(MainActivity.this, News.class));
+            finish();
+        }
     }
 }
